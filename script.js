cca3401ac060c45fb6e84f0f137bed612f7b2cf5
manojmksmanu <manoj2022019@gmail.com>
2024-06-23 21:28:32 +0530

    navbar sidebar and cards design done

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slidingSection = document.getElementById("slidingSection");
+  const toggleButton = document.getElementById("toggleButton");
+  const arrow = document.getElementById("arrow");
+  const slidingcontent= document.getElementById("sliding_content")
+  toggleButton.addEventListener("click", () => {
+    if (slidingSection.style.left === "0px") {
+      slidingSection.style.left = "-260px"; // 
+      arrow.style.transform = "rotate(0deg)"; // Arrow points to the right
+     slidingcontent.style.visibility = "hidden";
+    } else {
+      slidingSection.style.left = "0px";
+      arrow.style.transform = "rotate(180deg)"; // Arrow points to the left
+       slidingcontent.style.visibility = "visible";
+    }
+  });
+});
